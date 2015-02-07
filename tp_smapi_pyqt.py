a90c6c82c3487211b4591e188ec67513ba2fef8e
Nikola Kovacevic <nikolak@outlook.com>
2015-02-07 18:10:50 +0100

    Initial code commit

--- tp_smapi_pyqt.py
+++ tp_smapi_pyqt.py
@@ -33,7 +33,68 @@ class TPSmapiGUI(QtGui.QMainWindow, design.Ui_MainWindow):
     def __init__(self, parent=None):
         super(TPSmapiGUI, self).__init__(parent)
         self.setupUi(self)
+        self.current_battery = "BAT0"
+        self.values = {
+            "installed":self.installed,
+            "state":self.state,
+            "../ac_connected":self.ac_connected,
+            "cycle_count":self.cycle_count,
+            "current_now":self.current_now, # instantaneous current
+            "current_avg":self.current_avg, # last minute average
+            "power_now":self.power_now,   # instantaneous power
+            "power_avg":self.power_avg,   # last minute average
+            "last_full_capacity":self.last_full_capacity,
+            "remaining_percent":self.remaining_percent,
+            "remaining_running_time":self.remaining_running_time,
+            "remaining_charging_time":self.remaining_charge_time,
+            "remaining_capacity":self.remaining_capacity,
+            "design_capacity":self.design_capacity,
+            "voltage":self.voltage,
+            "design_voltage":self.design_voltage,
+            "manufacturer":self.manufacturer,
+            "model":self.model,
+            "barcoding":self.barcoding,
+            "chemistry":self.chemistry,
+            "serial":self.serial,
+            "manufacture_date":self.manufacturing_date,
+            "first_use_date":self.first_use_date,
+            "temperature":self.temperature # in milli-Celsius
+        }
 
+        self.populate_values()
+        self.get_charge_thresholds()
+        self.batteryComboBox.currentIndexChanged.connect(self.__change_battery)
+
+    def __change_battery(self):
+        self.current_battery = str(self.batteryComboBox.currentText())
+        self.populate_values()
+        self.get_charge_thresholds()
+
+
+    def _get_value(self, value_key, default_return = "N/a"):
+        base = "/sys/devices/platform/smapi/{}/".format(self.current_battery)
+        try:
+            with open(base+value_key) as input_file:
+                return input_file.read().rstrip()
+        except:
+            return default_return
+
+    # noinspection PyTypeChecker
+    def get_charge_thresholds(self):
+        start_threshold = int(self._get_value("start_charge_thresh", 0))
+        stop_threshold = int(self._get_value("stop_charge_thresh", 0))
+        inhibit_charge_min = int(self._get_value("inhibit_charge_minutes", 0))
+        self.start_charge_slider.setValue(start_threshold)
+        self.stop_charge_slider.setValue(stop_threshold)
+
+    def populate_values(self):
+        for value_key, value_label in self.values.items():
+            value = self._get_value(value_key)
+            if value == "0":
+                value = "False"
+            elif value == "1":
+                value = "True"
+            value_label.setText(value)
 
 def main():
     app = QtGui.QApplication(sys.argv)
